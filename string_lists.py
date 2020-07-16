4e6b166aefeab00efc2d5e17eec9d9746198edfe
ukates <derrickcates@icloud.com>
2020-07-16 17:48:48 -0400

    changed comment on string_lists.py

--- string_lists.py
+++ string_lists.py
@@ -6,8 +6,6 @@ reversed_string = "".join(reversed(user_string))
 
 print('is palindrome') if user_string == reversed_string else print('not palindrome')
 
-# Alternate
-
 alt_Reversed = user_string[::-1]
 
 print('is palindrome') if user_string == alt_Reversed else print('not palindrome')
