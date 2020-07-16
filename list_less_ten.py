9e93518aca6774c192728ca2df6ee275b5c01721
ukates <derrickcates@icloud.com>
2020-07-16 14:17:18 -0400

    adding list_less_ten.py

---
+++ list_less_ten.py
@@ -0,0 +1,14 @@
+# Take a list and write a program that prints out all elements of the list that are less than 5
+# Extra: make new list that has all elements and print out list, write in one line, ask user for number to check less
+
+
+a_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+num = 10
+
+for a in a_list:
+    if a < num:
+        print(a)
+
+b_list = [a for a in a_list if a < num]
+
+print(b_list)
